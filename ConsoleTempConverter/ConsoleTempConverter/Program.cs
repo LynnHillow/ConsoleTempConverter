08f06f46f61f53e4f979194aa7b06d3fe61c82a3
Lynn Hillow <lynnhillow@gmail.com>
2019-04-04 20:28:48 -0400

    Add files via upload

---
+++ ConsoleTempConverter/ConsoleTempConverter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTempConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            //string selection = "";
+            string Temp = "";
+            do
+            {
+                Console.WriteLine("Would you like the value converted to Celsius(C) or Fahrenheit(F)?" + "Enter C or F");
+                input = Console.ReadLine();
+
+                Console.WriteLine("Enter a temperature: ");
+                Temp = Console.ReadLine();
+                 
+                 if (input == "C")
+                 {
+                    double Celsius = (double.Parse(Temp) - 32) * 5 / 9;
+                    Console.WriteLine($"Celsius Temperature: {Celsius}C");
+                    continue;
+                 }
+
+                else if (input == "F")
+                {
+                    double Fahrenheit = (double.Parse(Temp) * 9 / 5) + 32;
+                    Console.WriteLine($"Fahrenheit Temperature: {Fahrenheit}F");
+                    continue;
+                }
+                
+            } while (input != "Exit");
+        }
+    }
+}
+    
+
+
+                      
+          
+       
+           
+                
+                            
+        
+    
+    
+
